322c5475d362c50902333b5f867f2bcfe51b2321
Geb <gch910@gmail.com>
2020-11-30 13:44:45 -0600

    remove buttons created, set and display cart functions done

--- storage-project.js
+++ storage-project.js
@@ -1,15 +1,40 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  let count = 0;
+  const form = document.querySelector(".form");
+  const selectItems = document.getElementById("items");
+  const shoppingCart = document.getElementById("shopping-cart");
+  const quantity = document.getElementById("quantity");
+  const ul = document.createElement("ul");
 
-  // const showCart = () => {
+  const showCart = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+      if (!localStorage.getItem(localStorage.key(i))) {
+        localStorage.setItem(localStorage.key(i), "no amount specified");
+      }
+      const button = document.createElement("button");
+      button.innerHTML = "Remove";
+      button.className = 'remove'
+      const li = document.createElement("li");
+      li.innerHTML = `${localStorage.key(i)} : ${localStorage.getItem(
+        localStorage.key(i)
+      )}`;
+      li.appendChild(button);
+      ul.appendChild(li);
+    }
 
-  // };
+    shoppingCart.appendChild(ul);
+  };
 
-  // const storeItem = () => {
+  const storeItem = () => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      count++;
+      localStorage.setItem(selectItems.value, quantity.value);
+    });
+  };
 
-  // };
+  const removeItem = () => {};
 
-  // const removeItem = () => {
-
-  // };
-
+  storeItem();
+  showCart();
 });
